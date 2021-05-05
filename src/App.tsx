5fb4191c12510ab3cfb47d5c215b088d01cc8f99
jhongarzon <jhongarzon@omella.com>
2021-05-04 23:33:09 -0500

    Feat: Add basic initial page and components

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { hot } from "react-hot-loader";
+import React, { FC } from "react";
+
+const App: FC = () => {
+  return <div>On24 Calculator code challenge</div>;
+};
+
+export default hot(module)(App);
